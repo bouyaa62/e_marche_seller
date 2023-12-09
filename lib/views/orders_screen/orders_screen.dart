2aa91173c3e4b53ab525a2d6a5d2edab4ff2b860
bouyaa62 <a.kaddour@esi-sba.dz>
2023-12-09 18:27:50 +0100

    orders backend done

--- lib/views/orders_screen/orders_screen.dart
+++ lib/views/orders_screen/orders_screen.dart
@@ -1,7 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:emart_seller/const/const.dart';
+import 'package:emart_seller/controllers/orders_controller.dart';
+import 'package:emart_seller/services/store_services.dart';
 import 'package:emart_seller/views/orders_screen/order_details.dart';
 import 'package:emart_seller/views/widgets/appbar_widget.dart';
+import 'package:emart_seller/views/widgets/loading_indicator.dart';
 import 'package:emart_seller/views/widgets/text_style.dart';
+import 'package:get/get.dart';
 import 'package:get/route_manager.dart';
 import 'package:intl/intl.dart' as intl;
 
@@ -10,54 +15,70 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var controller = Get.put(OrdersController());
     return Scaffold(
       appBar: appbarWidget(orders),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: List.generate(
-              20,
-              (index) => ListTile(
-                onTap: () {
-                  Get.to(() => const OrderDetails());
-                },
-                tileColor: textfieldGrey,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12)),
-                title: boldText(text: "9012364795479547", color: fontGrey),
-                subtitle: Column(
-                  children: [
-                    Row(
-                      children: [
-                        const Icon(Icons.calendar_month, color: fontGrey),
-                        10.widthBox,
-                        boldText(
-                            text: intl.DateFormat()
-                                .add_yMd()
-                                .format(DateTime.now()),
-                            color: fontGrey),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.payment,
-                          color: fontGrey,
-                        ),
-                        10.widthBox,
-                        boldText(text: unpaid, color: red),
-                      ],
-                    )
-                  ],
+      body: StreamBuilder(
+        stream: StoreServices.getOrders(currentUser!.uid),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return loadingIndicator();
+          } else {
+            var data = snapshot.data!.docs;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: List.generate(data.length, (index) {
+                    var time = data[index]['order_date'].toDate();
+                    return ListTile(
+                      onTap: () {
+                        Get.to(() => OrderDetails(
+                              data: data[index],
+                            ));
+                      },
+                      tileColor: textfieldGrey,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      title: boldText(
+                          text: "${data[index]['order_code']}",
+                          color: fontGrey),
+                      subtitle: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(Icons.calendar_month, color: fontGrey),
+                              10.widthBox,
+                              boldText(
+                                  text:
+                                      intl.DateFormat().add_yMd().format(time),
+                                  color: fontGrey),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.payment,
+                                color: fontGrey,
+                              ),
+                              10.widthBox,
+                              boldText(text: unpaid, color: red),
+                            ],
+                          )
+                        ],
+                      ),
+                      trailing: boldText(
+                          text: "\$${data[index]['total_ammount']}",
+                          color: purpleColor,
+                          size: 16.0),
+                    ).box.margin(const EdgeInsets.only(bottom: 4)).make();
+                  }),
                 ),
-                trailing:
-                    boldText(text: "\$40.0", color: purpleColor, size: 16.0),
-              ).box.margin(const EdgeInsets.only(bottom: 4)).make(),
-            ),
-          ),
-        ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
